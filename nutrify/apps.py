1aae7aec60a1ef2d9e853af4e3d75f492a3214ed
Gaganrajdeep Singh <gaganrajdeepsingh009@gmail.com>
2022-01-11 12:29:44 +0530

    Changed app name

--- nutrify/apps.py
+++ nutrify/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SkeletonwebsiteConfig(AppConfig):
+class nutrifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SkeletonWebsite'
+    name = 'nutrify'
